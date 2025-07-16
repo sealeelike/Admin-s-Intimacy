14e348a4bd494395682c3fef9796f928a743c0cb
sealeelike <1887415157aasvas@gmail.com>
2025-07-16 23:00:03 +0800

    add Google Search Console

---
+++ cld.cmd
@@ -0,0 +1,8 @@
+@echo off
+cd /d "%~dp0"
+set http_proxy=http://127.0.0.1:7890
+set https_proxy=http://127.0.0.1:7890
+set ANTHROPIC_BASE_URL=https://anyrouter.top
+set ANTHROPIC_AUTH_TOKEN=sk-oqSKnunddlfvlyTZsaNiA7Ux60439sT74ZLRJEhRW6DS7gGX
+claude
+pause
